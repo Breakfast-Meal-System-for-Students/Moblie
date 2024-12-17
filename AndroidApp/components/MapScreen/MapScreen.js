48c1ff1171a52dc7f7e67bf6cbbfb9554022ae6c
XTDV2021 <94182550+XTDV2021@users.noreply.github.com>
2024-12-17 20:20:02 +0700

    Maps

--- AndroidApp/components/MapScreen/MapScreen.js
+++ AndroidApp/components/MapScreen/MapScreen.js
@@ -40,7 +40,7 @@ const MapScreen = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "X-Goog-Api-Key": "AlzaSyMLWSaQgJYCT2ittsrZkpWQvDoV_AuPRYE", // Replace with your API Key
+            "X-Goog-Api-Key": "AlzaSyCF_c8sBQrX-S62lRLUMh6CQbn_0UKuN5G", // Replace with your API Key
           },
           body: JSON.stringify({
             origin: {
@@ -178,7 +178,7 @@ const MapScreen = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "X-Goog-Api-Key": "AlzaSyMLWSaQgJYCT2ittsrZkpWQvDoV_AuPRYE", // Replace with your API Key
+            "X-Goog-Api-Key": "AlzaSyCF_c8sBQrX-S62lRLUMh6CQbn_0UKuN5G", // Replace with your API Key
           },
           body: JSON.stringify({
             origin: {
@@ -231,7 +231,7 @@ const MapScreen = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "X-Goog-Api-Key": "AlzaSyMLWSaQgJYCT2ittsrZkpWQvDoV_AuPRYE", // Replace with your API Key
+            "X-Goog-Api-Key": "AlzaSyCF_c8sBQrX-S62lRLUMh6CQbn_0UKuN5G", // Replace with your API Key
           },
           body: JSON.stringify({
             origin: {
